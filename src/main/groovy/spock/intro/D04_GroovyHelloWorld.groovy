1b1db6dd2ec8b4df4c28b8acdd36a4b8d386e7b0
gilzilberfeld <gil.zilberfeld@gmail.com>
2019-06-06 12:10:28 +0300

    Added Groovy examples

---
+++ src/main/groovy/spock/intro/D04_GroovyHelloWorld.groovy
@@ -0,0 +1,7 @@
+package spock.intro
+
+class D04_GroovyHelloWorld {
+	static main(String[] args) {
+		println "Hello World"
+	}
+}
---
+++ src/main/groovy/spock/intro/D05_GroovyFeatures.groovy
@@ -0,0 +1,32 @@
+package spock.intro
+
+import groovy.json.JsonSlurper
+
+class D05_GroovyFeatures {
+	
+	def someStrings() {
+		def name1 = 'Cap'
+		def name2 = 'Tony'
+		
+		// string interpolation
+		def teamStatus= 
+			"${name1} and ${name2} are avengers." 
+		
+		// assert is part of the standard language
+		assert teamStatus == 'Cap and Tony are avengers.'
+	}
+	
+	def someJson() {
+		def jsonSlurper = new JsonSlurper()
+		def cap = jsonSlurper.parseText('{ "name": "Steve Rogers" }') 
+		
+		// Easy Json navigation
+		assert cap instanceof Map
+		assert cap.name == 'Steve Rogers'
+		
+		// Easy parsing
+		def appearances = jsonSlurper.parseText('{ "movies": [1,2,3,4] }') 
+		assert appearances.movies instanceof List 
+		assert appearances.movies == [1,2,3,4]
+	}
+}
